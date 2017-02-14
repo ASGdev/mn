815de536fcfacc31687f16894c8200d8a9335ae0
aurélien-win <aurelien.surier@gmail.com>
2017-02-14 14:18:33 +0100

    Add TODO

--- dot.c
+++ dot.c
@@ -1,4 +1,4 @@
-#include "mnblas.h"
+//#include "mnblas.h"
 
 float mncblas_sdot(const int N, const float *X, const int incX, const float *Y, const int incY){
     register unsigned int i = 0 ;
@@ -18,17 +18,18 @@ double mncblas_ddot(const int N, const double *X, const int incX, const double *
     register double dot = 0.0 ;
     for (; ((i < N) && (j < N)) ; i += incX, j+=incY){
         dot = dot + X [i] * Y [j] ;
+
     }
 
     return dot;
 }
 
-void   mncblas_cdotu_sub(const int N, const void *X, const int incX, const void *Y, const int incY, void *dotu){
-  /* a completer */
+void mncblas_cdotu_sub(const int N, const void *X, const int incX, const void *Y, const int incY, void *dotu){
+  /* complex non conjugué */
 
     register unsigned int i = 0 ;
     register unsigned int j = 0 ;
-    
+
     register double dot = 0.0 ;
     for (; ((i < N) && (j < N)) ; i += incX, j+=incY){
         dot = dot + X [i] * Y [j] ;
@@ -38,7 +39,7 @@ void   mncblas_cdotu_sub(const int N, const void *X, const int incX, const void 
 }
 
 void   mncblas_cdotc_sub(const int N, const void *X, const int incX, const void *Y, const int incY, void *dotc){
-  /* a completer */
+  /* a completer conjugué */
 
     return ;
 }

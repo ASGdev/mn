305e836ae9e5990c94af06570b0af87956354328
aurélien-win <aurelien.surier@gmail.com>
2017-02-14 15:13:19 +0100

    Updated dotc

--- dot.c
+++ dot.c
@@ -1,4 +1,5 @@
 //#include "mnblas.h"
+#include "mncommon.h"
 
 float mncblas_sdot(const int N, const float *X, const int incX, const float *Y, const int incY){
     register unsigned int i = 0 ;
@@ -20,7 +21,6 @@ double mncblas_ddot(const int N, const double *X, const int incX, const double *
         dot = dot + X [i] * Y [j] ;
 
     }
-
     return dot;
 }
 
@@ -30,15 +30,44 @@ void mncblas_cdotu_sub(const int N, const void *X, const int incX, const void *Y
     register unsigned int i = 0 ;
     register unsigned int j = 0 ;
 
-    register double dot = 0.0 ;
-    for (; ((i < N) && (j < N)) ; i += incX, j+=incY){
-        dot = dot + X [i] * Y [j] ;
+    float *XP = (float *) X;
+    float *YP = (float *) Y;
+
+    register float dotR = 0.0 ;
+    register float dotI = 0.0;
+    for (; ((i < N*2) && (j < N*2)) ; i += incX, j+=incY){
+        dotR = dotR + ((XP[i]*YP[j])-(XP[i+1]*YP[j+1]));
+        dotI = dotI + ((XP[i]*YP[j+1])+(XP[i+1]*YP[j]));
     }
 
+    dcomplexe R;
+    R.REEL = dotR;
+    R.IMAG = dotI;
+    *((dcomplexe*)dotu) = R;
     return ;
 }
 
 void   mncblas_cdotc_sub(const int N, const void *X, const int incX, const void *Y, const int incY, void *dotc){
+
+    register unsigned int i = 0 ;
+    register unsigned int j = 0 ;
+
+    float *XP = (float *) X;
+    float *YP = (float *) Y;
+
+    register float dotR = 0.0 ;
+    register float dotI = 0.0;
+    for (; ((i < N*2) && (j < N*2)) ; i += incX, j+=incY){
+        dotR = dotR + ((XP[i]*YP[i])-(XP[i+1]*YP[j+1]));
+        dotI = dotI + ((XP[i]*YP[j+1])+(XP[i+1]*YP[i]));
+    }
+
+    dcomplexe R;
+    R.REEL = dotR;
+    R.IMAG = dotI;
+    *((dcomplexe*)dotu) = R;
+    return ;
+}
   /* a completer conjugué */
 
     return ;
@@ -57,5 +86,6 @@ void   mncblas_zdotc_sub(const int N, const void *X, const int incX, const void 
 }
 
 
-
+int main(){
+}
 
